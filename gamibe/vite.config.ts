be6b3e2b1d84a97d80b44dbdccc7a04f29a29ce7
Jose Tannous <tagastudios@gmail.com>
2024-03-18 01:12:03 -0400

    Update favicon and app icons

--- gamibe/vite.config.ts
+++ gamibe/vite.config.ts
@@ -25,22 +25,22 @@ export default defineConfig({
                 start_url: '/',
                 icons: [
                     {
-                        src: '/icon-192x192.png',
+                        src: '/logo_192.png',
                         sizes: '192x192',
                         type: 'image/png'
                     },
                     {
-                        src: '/icon-256x256.png',
+                        src: '/logo_256.png',
                         sizes: '256x256',
                         type: 'image/png'
                     },
                     {
-                        src: '/icon-384x384.png',
+                        src: '/logo_384.png',
                         sizes: '384x384',
                         type: 'image/png'
                     },
                     {
-                        src: '/icon-512x512.png',
+                        src: '/logo_512.png',
                         sizes: '512x512',
                         type: 'image/png'
                     }
